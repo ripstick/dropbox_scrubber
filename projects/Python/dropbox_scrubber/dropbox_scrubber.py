0210a62e354b6ee342553c9bc485679ecd81cce7
ripstick <wilsonjs0311@gmail.com>
2014-09-22 20:57:14 -0700

    consolidated and cleaned up

--- projects/Python/dropbox_scrubber/dropbox_scrubber.py
+++ projects/Python/dropbox_scrubber/dropbox_scrubber.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env
-# adapted from www.thinkpython.com example
 import os
 
-count = 0
 dup_files = {}
-
-#for (dirname, dirs, files) in os.walk('.'):
- # for filename in files:
-   # count += 1
-    #thefile = os.path.join(dirname,filename)
-    #file_int = 1
-    #thesize = str(os.path.getsize(thefile)/1000) + 'k'
-     #print thesize , thefile
-
-#print 'Files: ', count
 
 for dirname, dirs, files in os.walk(','):
   for filename in files:
-    thefile = os.path.join(dirname, filename)
-    file_int = 1
-    if filename not in dup_files:
-      dup_files[filename] = file_int
+    dup_files[filename] = dup_files.get(filename, 0) + 1
+
+print dup_files
