40d0a9bb3c000ac36e6aefe56b91c1751dc3e758
ripstick <wilsonjs0311@gmail.com>
2014-09-22 20:38:58 -0700

    start changing structure

--- projects/Python/dropbox_scrubber/dropbox_scrubber.py
+++ projects/Python/dropbox_scrubber/dropbox_scrubber.py
@@ -3,11 +3,21 @@
 import os
 
 count = 0
-for (dirname, dirs, files) in os.walk('.'):
-  for filename in files:
-    count += 1
-    thefile = os.path.join(dirname,filename)
-    thesize = str(os.path.getsize(thefile)/1000) + 'k'
-    print thesize , thefile
+dup_files = {}
 
-print 'Files: ', count
+#for (dirname, dirs, files) in os.walk('.'):
+ # for filename in files:
+   # count += 1
+    #thefile = os.path.join(dirname,filename)
+    #file_int = 1
+    #thesize = str(os.path.getsize(thefile)/1000) + 'k'
+     #print thesize , thefile
+
+#print 'Files: ', count
+
+for dirname, dirs, files in os.walk(','):
+  for filename in files:
+    thefile = os.path.join(dirname, filename)
+    file_int = 1
+    if filename not in dup_files:
+      dup_files[filename] = file_int
