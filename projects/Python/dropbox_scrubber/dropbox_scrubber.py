7d3178ff9642e9e0cead5e9e6ec747b3ea59b0f8
ripstick <wilsonjs0311@gmail.com>
2014-09-23 20:15:39 -0700

    add write to file and >1 screen

--- projects/Python/dropbox_scrubber/dropbox_scrubber.py
+++ projects/Python/dropbox_scrubber/dropbox_scrubber.py
@@ -10,7 +10,10 @@ for dirname, dirs, files in os.walk('.'):
       else:
            filelist[filename] = 1
 
+f = open('results.txt','w')
 for w in sorted(filelist, key=filelist.get, reverse=True):
-    print w, filelist[w]
+     if filelist[w] > 1:
+         f.write(w + ' ' + str(filelist[w])+"\n")
 
+f.close
 # TODO: screen for counts > 1 and write to a .txt file
